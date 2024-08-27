7f497b5c1d0d51234ac8ee24b09dc38755caf8b6
JH.Y <frosrain@naver.com>
2024-08-27 14:51:33 +0900

    Stoppint for Maya Installing

---
+++ Source/Magician/GM_MainBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_MainBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICIAN_API AGM_MainBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
